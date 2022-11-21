832fb6190b67a28da24e6ab95387b4a25286de99
antoine carpentier <a.carpentier.wcs@gmail.com>
2022-11-21 09:34:02 +0100

    first commit

---
+++ src/main/java/com/wildcodeschool/coffreFort/CoffreFortApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.coffreFort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffreFortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffreFortApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/coffreFort/CoffreFortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.coffreFort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffreFortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
